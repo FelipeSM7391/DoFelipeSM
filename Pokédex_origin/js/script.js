8f6d3597d51085e9e830b2e8649a596ed767b89a
Felipe Satoru Meucci <2025311470@aluno.riogrande.ifrs.edu.br>
2026-04-14 22:26:45 -0300

    script.js

--- Pokédex_origin/js/script.js
+++ Pokédex_origin/js/script.js
@@ -1,4 +1,22 @@
-/*const fetchPokemon = async(pokemon) {
-    const = APIResponse = await fetch('https://pokeapi.co/api/v2/pokemon/${pokemon}');
+const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumb = document.querySelector('.pokemon_num');
+const pokemonImg = document.querySelector('.pokemon_image');
+const form = document.querySelector('.form');
 
-}*/
+const fetchPokemon = async (pokemon)  => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const data = await APIResponse.json();
+    return data;
+}
+
+    const renderpkmon = async (pokemon) => {
+
+        const data = await fetchPokemon(pokemon);
+
+        pokemonName.innerHTML = data.name
+        pokemonNumb.innerHTML = data.id
+        pokemonImg.src = data[`sprites`][`versions`][`generation-v`][`black-white`][`animated`][`front_default`];
+}
+
+//form.addEventListener(`submit`, funcao)
+renderpkmon(376)
