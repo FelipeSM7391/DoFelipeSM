ee8db2ecb598af4a564f40c03af56fb1c022b786
Felipe Satoru Meucci <2025311470@aluno.riogrande.ifrs.edu.br>
2026-04-15 22:24:21 -0300

    script.js

--- Pokédex_origin/js/script.js
+++ Pokédex_origin/js/script.js
@@ -1,22 +1,42 @@
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumb = document.querySelector('.pokemon_num');
 const pokemonImg = document.querySelector('.pokemon_image');
+
 const form = document.querySelector('.form');
+const input = document.querySelector(`.input_search`);
 
 const fetchPokemon = async (pokemon)  => {
     const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const data = await APIResponse.json();
-    return data;
+
+    if (APIResponse.status ==200){
+        const data = await APIResponse.json();
+        return data;
+    }
 }
 
-    const renderpkmon = async (pokemon) => {
+const renderpkmon = async (pokemon) => {
+
+        pokemonName.innerHTML = `Loading...`
+        pokemonNumb.innerHTML = ``
 
         const data = await fetchPokemon(pokemon);
 
-        pokemonName.innerHTML = data.name
-        pokemonNumb.innerHTML = data.id
-        pokemonImg.src = data[`sprites`][`versions`][`generation-v`][`black-white`][`animated`][`front_default`];
+        if (data) {
+            pokemonName.innerHTML = data.name
+            pokemonNumb.innerHTML = data.id
+            pokemonImg.src = data[`sprites`][`versions`][`generation-v`][`black-white`][`animated`][`front_default`];
+            input.value = ``;
+        }else {
+            pokemonName.innerHTML = `Not Found :/`
+            pokemonNumb.innerHTML = `???`
+            input.value = ``;
+        }
+
 }
 
-//form.addEventListener(`submit`, funcao)
-renderpkmon(376)
+form.addEventListener(`submit`, (event) => {
+
+    event.preventDefault();
+
+    renderpkmon(input.value.toLowerCase());
+});
